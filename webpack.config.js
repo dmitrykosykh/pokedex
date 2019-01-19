f238c715fc877d13e48f15da70e635b4eac6a1f4
Dmitry Kosykh <dmitry.kosyh@gmail.com>
2019-01-19 16:15:42 +0500

    updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const webpack = require('webpack');
 
 const config = {
   entry: './src/index.jsx',
@@ -70,6 +71,32 @@ module.exports = (env, argv) => {
     config.devtool = false;
     config.mode = 'production';
     config.entry = './src/index.prod.jsx';
+    config.optimization = {
+      runtimeChunk: 'single',
+      splitChunks: {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 0,
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name(module) {
+              // get the name. E.g. node_modules/packageName/not/this/part.js
+              // or node_modules/packageName
+              const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+
+              // npm package names are URL-safe, but some servers don't like @ symbols
+              return `npm.${packageName.replace('@', '')}`;
+            },
+          },
+        },
+      },
+    };
+    config.output = {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].[contenthash].js',
+    };
+    config.plugins.push(new webpack.HashedModuleIdsPlugin());
   } else if (env.development) {
     config.devtool = 'source-map';
     config.mode = 'development';
